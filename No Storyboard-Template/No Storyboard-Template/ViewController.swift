11599f936bd97ae2bf074bbabdb8ab14f63297a7
PatrikCesnek <46940455+PatrikCesnek@users.noreply.github.com>
2020-02-04 19:47:43 +0100

    Add template code
    Template code for programmatic UI for iOS 13

---
+++ No Storyboard-Template/No Storyboard-Template/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  No Storyboard-Template
+//
+//  Created by Patrik Cesnek on 04/02/2020.
+//  Copyright © 2020 Patrik Cesnek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
